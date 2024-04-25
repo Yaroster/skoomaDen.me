0eb05c71335c44118828e6348d89977185678b6a
Ilyes <yaroster2@gmail.com>
2024-04-25 03:40:51 +0200

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import { THEME_CONFIG } from "./src/theme.config";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  base: '/skoomaDen/',  // Set the base URL for the project
+  site: THEME_CONFIG.website,
+  prefetch: true,
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      langs: [],
+      wrap: true,
+    },
+  },
+  integrations: [
+    UnoCSS({
+      injectReset: true
+    }),
+    robotsTxt(),
+    sitemap(),
+    mdx()
+  ]
+});
