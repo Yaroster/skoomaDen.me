954e9b7089882b39794065869d41990812131bc6
Ilyes <yaroster2@gmail.com>
2024-04-28 22:41:33 +0200

    Added disqus

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -59,9 +59,9 @@ export const THEME_CONFIG: App.Locals['config'] = {
   ],
   /** your comment provider */
   comments: {
-    // disqus: {
-    //   shortname: "typography-astro",
-    // },
+    disqus: {
+      shortname: "typography-astro",
+    },
     // giscus: {
     //   repo: 'moeyua/astro-theme-skoomaDen',
     //   repoId: 'R_kgDOKy9HOQ',
@@ -76,9 +76,9 @@ export const THEME_CONFIG: App.Locals['config'] = {
     //   lang: 'en-us',
     //   loading: 'lazy',
     // },
-    twikoo: {
-      envId: "https://twikoo-tau-flame.vercel.app",
-    }
+    // twikoo: {
+    //   envId: "https://twikoo-tau-flame.vercel.app",
+    // }
   }
 }
 
