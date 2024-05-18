191b5ee10caaaeeec5507286633e900a81ff84e6
Yaroster <yaroster2@gmail.com>
2024-05-18 19:56:00 +0200

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,55 +3,58 @@ import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from 'markdown-it'
 
 export async function getCategories() {
-  const posts = await getPosts()
+  const posts = await getPosts();
 
-  const categories = new Map<string, Post[]>()
+  const categories = new Map<string, Post[]>();
 
   posts.forEach((post) => {
     if (post.data.categories) {
       post.data.categories.forEach((c) => {
-        const posts = categories.get(c) || []
-        posts.push(post)
-        categories.set(c, posts)
-      })
+        const posts = categories.get(c) || [];
+        posts.push(post);
+        categories.set(c, posts);
+      });
     }
-  })
+  });
 
-  return categories
+  // Sort categories alphabetically by category name
+  const sortedCategories = new Map([...categories.entries()].sort());
+
+  return sortedCategories;
 }
 
 export async function getPosts() {
-  const posts = await getCollection('posts')
+  const posts = await getCollection('posts');
   posts.sort((a, b) => {
-    const aDate = a.data.pubDate || new Date()
-    const bDate = b.data.pubDate || new Date()
-    return bDate.getTime() - aDate.getTime()
-  })
-  return posts
+    const aDate = a.data.pubDate || new Date();
+    const bDate = b.data.pubDate || new Date();
+    return bDate.getTime() - aDate.getTime();
+  });
+  return posts;
 }
 
-const parser = new MarkdownIt()
+const parser = new MarkdownIt();
 
 export function getPostDescription(post: Post) {
   if (post.data.description) {
-    return post.data.description
+    return post.data.description;
   }
 
-  const html = parser.render(post.body)
-  const sanitized = sanitizeHtml(html, { allowedTags: [] })
-  return sanitized.slice(0, 400)
+  const html = parser.render(post.body);
+  const sanitized = sanitizeHtml(html, { allowedTags: [] });
+  return sanitized.slice(0, 400);
 }
 
 export function formatDate(date?: Date) {
-  if(!date) return '--'
-  const year = date.getFullYear().toString().padStart(4, '0')
-  const month = (date.getMonth() + 1).toString().padStart(2, '0')
-  const day = date.getDate().toString().padStart(2, '0')
+  if (!date) return '--';
+  const year = date.getFullYear().toString().padStart(4, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
 
-  return `${year}-${month}-${day}`
+  return `${year}-${month}-${day}`;
 }
 
-export function getPathFromCategory(category: string, category_map: {name: string, path: string}[]) {
-  const mappingPath = category_map.find(l => l.name === category)
-  return mappingPath ? mappingPath.path : category
+export function getPathFromCategory(category: string, category_map: { name: string, path: string }[]) {
+  const mappingPath = category_map.find(l => l.name === category);
+  return mappingPath ? mappingPath.path : category;
 }
