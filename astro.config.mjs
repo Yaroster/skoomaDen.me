09c8dd0552b7b898472fba41a6014707fcd0f23b
Ilyes <yaroster2@gmail.com>
2024-10-15 11:50:52 +0400

    Revert ","
    This reverts commit f6df852d5719f21602147607462fb7bf93fb7b68.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: "https://skoomaden.me",
@@ -21,8 +20,11 @@ export default defineConfig({
     sitemap(),
     react(),
   ],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  output: "static",
+  images: {
+    sharp: {
+      quality: 75,
+      format: 'webp',
+    },
+  },
 });
